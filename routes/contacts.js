9194c57b5efe6e8a342b9fb1eb593e0fc5222831
Israel Adetunji <adisen230@gmail.com>
2020-07-30 15:47:28 +0100

    Get all users contact added

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../middleware/auth");
 
-// @route   POST api/contacts
+const Contact = require("../model/Contact");
+const User = require("../model/User");
+
+// @route   GET api/contacts
 // @desc    Get all users contacts
 // @access  Private
-router.post("/", (req, res) => {
-  res.send("Get all contacts");
+router.get("/", auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+
+    res.json(contacts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
 });
 
 // @route   POST api/contacts
