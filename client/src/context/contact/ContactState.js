0331352a08507023cf5b0ca02d727bcbb636bfe9
Israel Adetunji <adisen230@gmail.com>
2020-08-05 21:53:27 +0100

    Contact form added

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from "react";
-import uuid from "uuid";
+import { v4 as uuidv4 } from "uuid";
 import contactContext from "./contactContext";
 import contactReducer from "./contactReducer";
 import {
@@ -42,6 +42,10 @@ const ContactState = props => {
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   // Add contact
+  const addContact = contact => {
+    contact.id = uuidv4;
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
 
   // Delete Contact
 
@@ -59,7 +63,9 @@ const ContactState = props => {
     <contactContext.Provider
       value={{
         contacts: state.contacts,
-      }}>
+        addContact,
+      }}
+    >
       {props.children}
     </contactContext.Provider>
   );
